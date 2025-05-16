0ac265e3482a7cca4338a5803171ee2fab9c9d78
JoseFRuiz <jfruizmu@gmail.com>
2025-05-16 06:52:48 -0500

    exp003 ready to run

--- run_experiment_gru_lightning.py
+++ run_experiment_gru_lightning.py
@@ -1,4 +1,5 @@
 # python run_experiment_gru_lightning.py --save_dir "gru_002" --epochs 100 --eval_interval 10 --lr 1e-3 --batch_size 100 --use_gpu
+# python run_experiment_gru_lightning.py --save_dir "gru_003" --epochs 100 --eval_interval 10 --lr 1e-3 --batch_size 100 --use_gpu
 
 import os
 import argparse
