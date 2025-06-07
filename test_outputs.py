0e9366e237e55680173f63bc55f6ed131638fab1
JoseFRuiz <jfruizmu@gmail.com>
2025-06-07 07:44:54 -0500

    update

---
+++ test_outputs.py
@@ -0,0 +1,6 @@
+# Check the outputs of the model
+# We can see the output norm.
+# Check the weights norm
+# Check the gradients norm
+# An alternative to binary cross entropy loss could be using a loss that computes the maximum instead of the mean
+
