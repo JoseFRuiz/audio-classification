e260507b6e6a47a47892400c2feeb59fc17d71d7
JoseFRuiz <jfruizmu@gmail.com>
2025-07-06 09:26:41 -0500

    update with pytorch module

--- gpu_job.sh
+++ gpu_job.sh
@@ -6,12 +6,14 @@
 #SBATCH --mail-user=jfruizmu@unal.edu.co
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=10
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks-per-socket=1
-#SBATCH --mem-per-cpu=20000mb
+#SBATCH --mem=180gb
 #SBATCH --distribution=cyclic:cyclic
-#SBATCH --partition=hpg-b200 #hpg-b200 # instead of gpu
+#SBATCH --partition=hpg-b200
+#SBATCH --qos=azare
+#SBATCH --account=azare
 #SBATCH --gres=gpu:1
 #SBATCH --time=96:00:00
 
@@ -19,8 +21,7 @@ echo "Date      = $(date)"
 echo "host      = $(hostname -s)"
 echo "Directory = $(pwd)"
 
-source activate audio-classification
+module load cuda/12.9.1
+module load pytorch
 
 python run_experiment_gru_lightning.py --save_dir "gru_022" --epochs 1000 --eval_interval 10 --log_interval 10 --lr 1e-2 --batch_size 100 --use_gpu --test_size 0.1 --dropout 0.1 --loss_fn "asymmetric" --gamma_pos 1.0 --gamma_neg 4.0
-
-conda deactivate
