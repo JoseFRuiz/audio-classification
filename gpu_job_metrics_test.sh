f888caf3a1336502a202a21f890be3a9cb80e628
JoseFRuiz <jfruizmu@gmail.com>
2025-07-18 16:35:58 -0500

    update

--- gpu_job_metrics_test.sh
+++ gpu_job_metrics_test.sh
@@ -15,7 +15,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --qos=azare
 #SBATCH --account=azare
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 
 echo "Date      = $(date)"
 echo "host      = $(hostname -s)"
